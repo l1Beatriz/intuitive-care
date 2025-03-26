fec4734e1ec8f901279b802b78829100c6fc82a0
Beatriz Santos <beatrizsantosmmg@gmail.com>
2025-03-26 16:47:43 -0300

    <feat> Creating requisition and searching for the list tag

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import requests 
+from bs4 import BeautifulSoup
+
+request = requests.get('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos')
+response_html = request.text
+
+soup = BeautifulSoup(response_html, 'html.parser')
+
+find_list = soup.find('ol')
+anchor_tag = find_list.find_all('a', class_='internal-link' )
+
+for link in anchor_tag: 
+    print(f'\nLink do Anexo PDF: \n{link.attrs['href']}\n\n')
+    
