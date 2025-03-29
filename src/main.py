6ee72044035d130d51d0203edc35fcfcf2b58c8b
Beatriz Santos <beatrizsantosmmg@gmail.com>
2025-03-29 10:38:49 -0300

    <refactor> accessing attachment links through keys for greater filter efficiency

--- src/main.py
+++ src/main.py
@@ -3,27 +3,45 @@ import zipfile
 import requests 
 from bs4 import BeautifulSoup
 
+URL = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
 
-find_list = soup.find('ol')
-anchor_tag = find_list.find_all('a', class_='internal-link' )
-# https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos
 def get_pdf_links(url):
-    response = requests.get('')
-    soup = BeautifulSoup(response, 'html.parser')
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f'Erro ao acessar a URL: {url}')
 
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = []
 
+    keywords = ['Anexo I', 'Anexo II']
 
-def download_files():
-    global href
-    for link in anchor_tag:
-        href = link.attrs['href'] 
+    for link in soup.find_all('a'):
+        href = link.get('href', '')
+        text_link = link.get_text(strip=True)
+        
+        if(href.endswith('.pdf') and any(key in text_link for key in keywords)):
+            if(href.startswith('/')):
+                href = f'https://www.gov.br{href}'
+                
+            print(f'\nLink do Anexo PDF: \n{href}\n')
+            links.append(href)
+    
+    return links
+
+# def download_files():
+    # global href
+    # for link in anchor_tag:
+        # href = link.attrs['href'] 
 
         # filter links ending in '.pdf'
-        if(href.endswith('pdf')):
-            print(f'\nLink do Anexo PDF: \n{href}\n')
-            url = requests.get(href)
-            print(url)
+        # if(href.endswith('pdf')):
+        #     url = requests.get(href)
+        #     print(url)
 
 
+def main():
+    get_pdf_links(URL)
 
-download_files()
+
+if __name__ == "__main__":
+    main()
