f6659c24b4c4593e4e93fae0e2196a0e81e62eb2
Beatriz Santos <beatrizsantosmmg@gmail.com>
2025-03-29 14:47:36 -0300

    <feat> creating function to compress downloaded files

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import zipfile
 import requests 
 from bs4 import BeautifulSoup
-from colorama import Fore, Back, Style
+from colorama import Fore, Style
 
 URL = 'https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos'
 DOWNLOADS_FOLDER = 'downloads'
@@ -21,20 +21,23 @@ def get_pdf_links(url):
         href = link.get('href', '')
         text_link = link.get_text(strip=True)
         
-        if(href.endswith('.pdf') and any(key.lower() in text_link.lower() for key in keywords)):
-            if(href.startswith('/')):
+        if href.endswith('.pdf') and any(key.lower() in text_link.lower() for key in keywords):
+            if href.startswith('/'):
                 href = f'https://www.gov.br{href}'
                 
-            print(f'\nLink do Anexo PDF: {Fore.GREEN}\n{href}\n{Style.RESET_ALL}')
+            # print(f'\nLink do Anexo PDF: {Fore.GREEN}\n{href}\n{Style.RESET_ALL}')
             links.append(href)
     
     return links
 
 def download_files(url, folder):
+    if not os.path.exists(DOWNLOADS_FOLDER):
+        os.makedirs(DOWNLOADS_FOLDER)
+
     file_name = os.path.join(folder, url.split('/')[-1])
 
     response = requests.get(url, stream=True)
-    if(response.status_code == 200):
+    if response.status_code == 200:
         with open(file_name, 'wb') as file:
             for chunk in response.iter_content(chunk_size=1024):
                 file.write(chunk)
@@ -42,15 +45,24 @@ def download_files(url, folder):
     else:
         print(f'{Fore.RED}Falha ao realizar download do arquivo: {url}{Style.RESET_ALL}')
 
-def main():
-    if not os.path.exists(DOWNLOADS_FOLDER):
-        os.makedirs(DOWNLOADS_FOLDER)
+def zip_files(folder, output_file_name='anexos.zip'):
+    zip_path = os.path.join(folder, output_file_name)
+    
+    with zipfile.ZipFile(zip_path, 'w') as zipf:
+        for file in os.path.listdir(folder):
+            file_path = os.path.join(folder, file)
+            if os.path.isfile(file_path):
+                zipf.write(file_path, os.path.basename(file_path))
+                
+    print(f'{Fore.MAGENTA}Arquivos compactados em:{Style.RESET_ALL}{zip_path}')
 
+
+def main():
     try:
         links_pdf = get_pdf_links(URL)
         print(f'Encontrados {len(links_pdf)} arquivos PDF para download\n')
 
-        if(not links_pdf):
+        if not links_pdf:
             print('Nenhum arquivo disponível')
             return
 
@@ -59,6 +71,7 @@ def main():
 
     except Exception as error:
         print(f'{Fore.RED}Erro{Style.RESET_ALL}', error)
+
 
 
 if __name__ == "__main__":
