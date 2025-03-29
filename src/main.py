35fecc65d63c6fb321095e5934ed3f3def0a02d2
Beatriz Santos <beatrizsantosmmg@gmail.com>
2025-03-29 09:51:05 -0300

    <refactor> creating new function for geting pdfs

--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
+import os
+import zipfile
 import requests 
 from bs4 import BeautifulSoup
 
-request = requests.get('https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos')
-response_html = request.text
-
-soup = BeautifulSoup(response_html, 'html.parser')
 
 find_list = soup.find('ol')
 anchor_tag = find_list.find_all('a', class_='internal-link' )
+# https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos
+def get_pdf_links(url):
+    response = requests.get('')
+    soup = BeautifulSoup(response, 'html.parser')
 
-for link in anchor_tag:
-    href = link.attrs['href'] 
 
-    # filter links ending in '.pdf'
-    if(href.endswith('pdf')):
-        print(f'\nLink do Anexo PDF: \n{link.attrs['href']}\n\n')
+
+def download_files():
+    global href
+    for link in anchor_tag:
+        href = link.attrs['href'] 
+
+        # filter links ending in '.pdf'
+        if(href.endswith('pdf')):
+            print(f'\nLink do Anexo PDF: \n{href}\n')
+            url = requests.get(href)
+            print(url)
+
+
+
+download_files()
